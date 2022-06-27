21896081cfe2553e56a61460106137e741178105
Juan Pablo Muñoz <munozsjp@gmail.com>
2022-06-27 23:16:54 +0200

    jenkinsFile

--- Jenkinsfile.node.groovy
+++ Jenkinsfile.node.groovy
@@ -1,8 +1,31 @@
-node('Jenkins-slave') {
-    stage('Source') {
-        git clone 'https://github.com/Voztr0/server.git'
-    }
-    stage('Build') {
-        echo 'Building stage!'
+pipeline {
+    agent 'Jenkins-slave'
+
+    stages {
+        stage('Source') {
+            steps {
+                echo 'get sources'
+            }
+        }
+        stage('Build') {
+            steps {
+                echo 'run build'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'run deploy'
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'run test'
+            }
+        }
+        stage('Feedback') {
+            steps {
+                echo 'send reports'
+            }
+        }
     }
 }
