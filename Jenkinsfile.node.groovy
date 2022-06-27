e7fc80b6f65ab052d9f9cb58d6f2ce281e4bccf4
Juan Pablo Muñoz <munozsjp@gmail.com>
2022-06-27 23:54:38 +0200

    fix Jenkinsfile3

--- Jenkinsfile.node.groovy
+++ Jenkinsfile.node.groovy
@@ -1,15 +1,15 @@
 pipeline {
     agent any
-        stages {
-            stage('build') {
-                steps {
-                    echo "Hello World!"
-                }
-            }
-            stage('Source') {
-                steps {
-                    git 'https://github.com/Voztr0/server.git'
-                }    
+    stages {
+        stage('build') {
+            steps {
+                echo "Hello World!"
             }
         }
+        stage('Source') {
+            steps {
+                git 'https://github.com/Voztr0/server.git'
+            }    
+        }
+    }
 }
