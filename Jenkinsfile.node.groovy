d326e4e7ce33d521a70432db1201c1ba090139a4
Juan Pablo Muñoz <munozsjp@gmail.com>
2022-06-27 23:44:37 +0200

    jenkinsFile

--- Jenkinsfile.node.groovy
+++ Jenkinsfile.node.groovy
@@ -11,5 +11,20 @@ pipeline {
                 git 'https://github.com/Voztr0/server.git'
             }    
         }
+        stage('ObtainRepo') {
+            steps {
+                git 'https://github.com/Voztr0/server.git'
+            }    
+        }
+        stage('install') {
+            steps {
+                sh 'npm install'
+            }    
+        }
+         stage('run') {
+            steps {
+                sh 'node src/index.js'
+            }    
+        }
     }
 }
