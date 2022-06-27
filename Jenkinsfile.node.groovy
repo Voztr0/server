94ab16236b3f650eeaaa8a3cb4e816b5cd3d0bf3
Juan Pablo Muñoz <munozsjp@gmail.com>
2022-06-27 23:18:51 +0200

    jenkinsFile

--- Jenkinsfile.node.groovy
+++ Jenkinsfile.node.groovy
@@ -1,30 +1,9 @@
 pipeline {
-    agent 'Jenkins-slave'
-
+    agent { jenkin-slave }
     stages {
-        stage('Source') {
+        stage('build') {
             steps {
-                echo 'get sources'
-            }
-        }
-        stage('Build') {
-            steps {
-                echo 'run build'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'run deploy'
-            }
-        }
-        stage('Test') {
-            steps {
-                echo 'run test'
-            }
-        }
-        stage('Feedback') {
-            steps {
-                echo 'send reports'
+                sh 'node --version'
             }
         }
     }
